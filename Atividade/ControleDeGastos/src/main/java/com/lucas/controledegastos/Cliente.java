bccc1a1f16151958ef4b7d8cb23f2a9b5e9d577d
TraPxEziO <39560660+TraPxEziO@users.noreply.github.com>
2018-07-16 17:51:40 -0300

    Add files via upload

---
+++ Atividade/ControleDeGastos/src/main/java/com/lucas/controledegastos/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lucas.controledegastos;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cliente {
+    
+    String codIdentificacao;
+    String email;
+    String nomeFantasia;
+    String razaoSocial;
+    String tipoCliente;
+    
+}
---
+++ Atividade/ControleDeGastos/src/main/java/com/lucas/controledegastos/Endereco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lucas.controledegastos;
+
+/**
+ *
+ * @author aluno
+ */
+public class Endereco {
+    
+    String bairro;
+    String cep;
+    String cidade;
+    String complemento;
+    String numero;
+    String rua;
+    String telefone1;
+    String telefone2;
+    
+}
